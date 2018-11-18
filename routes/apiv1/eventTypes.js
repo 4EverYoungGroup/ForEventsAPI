6bb95f6bc8e388b57f133bba66559168acd8defb
4EverYoungGroup <44073898+4EverYoungGroup@users.noreply.github.com>
2018-11-18 18:19:45 +0100

    Update eventTypes.js

--- routes/apiv1/eventTypes.js
+++ routes/apiv1/eventTypes.js
@@ -42,8 +42,8 @@ router.get('/', async (req, res, next) => {
 
 router.put('/:id', async (req,res, next) =>{
     const name = req.query.name;
-    const _id = req.param.id;
-        if (name && id){
+    const _id = req.params.id;
+        if (name && _id){
             try{
                 const eventTypeUpdated = await Event_type.findOneAndUpdate({_id: _id}, {$set: {name: name}}, {new:true}).exec();
                 res.status(200).json({ok: true, result: eventTypeUpdated});
